a79021faffaa120c7d9b7d8796428902bf3baa21
SufianHamdan <sufian_hamdan@yahoo.com>
2021-03-16 21:14:32 +0200

    lab03 Extend the project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let mark = 0;
 alert('Please answer the quistion by either typing yes or no, y or n');
 let favColor = prompt('Is black My favourite color?');
 
@@ -7,6 +7,8 @@ if(favColor.toLowerCase() === 'yes' || favColor.toLowerCase() === 'y')
 {
   alert('Correct, my favourite color is black');
   // console.log('Correct, my favourite color is black');
+  mark++;
+
 }
 
 else if(favColor.toLowerCase() === 'no' || favColor.toLowerCase() === 'n')
@@ -30,6 +32,8 @@ else if(dogCat.toLowerCase() === 'no' || dogCat.toLowerCase() === 'n')
 {
   alert('Correct, my favourite pet is a dog');
   // console.log('Correct, my favourite pet is a dog');
+  mark++;
+
 }
 
 else
@@ -43,6 +47,8 @@ if(summerWinter.toLowerCase() === 'yes' || summerWinter.toLowerCase() === 'y')
 {
   alert('Correct, my favourite season is Summer');
   // console.log('Correct, my favourite season is Summer');
+  mark++;
+
 }
 
 else if(summerWinter.toLowerCase() === 'no' || summerWinter.toLowerCase() === 'n')
@@ -66,6 +72,8 @@ else if(musicType.toLowerCase() === 'no' || musicType.toLowerCase() === 'n')
 {
   alert('Correct, my favourite music type is Rock and Roll');
   // console.log('Correct, my favourite music type is Rock and Roll');
+  mark++;
+
 }
 
 else
@@ -73,20 +81,78 @@ else
   alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
 }
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction();};
+let missyClean = prompt('Am i a missy person');
 
-// Get the navbar
-let navbar = document.getElementById('navbar');
+if(missyClean.toLowerCase() === 'yes' || missyClean.toLowerCase() === 'y')
+{
+  alert('Wrong, I am a Clean person');
+}
 
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
+else if(missyClean.toLowerCase() === 'no' || missyClean.toLowerCase() === 'n')
+{
+  alert('Correct, I am a Clean person');
+  // console.log('Correct, I am a Clean person');
+  mark++;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
+}
+
+else
+{
+  alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
+}
+
+
+let userNumber = prompt('Can you guess what is my lucky number between 1 - 100');
+let noAttempt = 4;
+while(noAttempt > 1)
+{
+  if(parseInt(userNumber) === 50)
+  {
+    mark++;
+    alert('Correct');
+    break;
+  }
+  else if(parseInt(userNumber) <= 40)
+  {
+    alert('Too Low');
+  }
+  else if(parseInt(userNumber) >= 60)
+  {
+    alert('Too High');
+  }
+  else
+  {
+    alert('invalid input');
+  }
+  noAttempt--;
+  userNumber = prompt('Can you guess what is my lucky number between 1 - 100, ' +noAttempt+' Attempt');
+}
+
+alert('Can you guess what language do i know');
+let attempts = 6;
+let myAnswer = ['c#','html','css','javascript','java', 'sql'];
+//let userAnswer = prompt('type the correct answer \n (c# HTML CSS Java Javascript python SQl PHP');
+
+for(let j = -5; j<attempts; j++)
+{
+  let userAnswer = prompt('type the correct answer \n (c# HTML CSS Java Javascript python SQl PHP '+attempts+' attempts left');
+  attempts--;
+  for(let i = 0; i<myAnswer.length; i++)
+  {
+    if(userAnswer === myAnswer[i])
+    {
+      alert('correct!');
+      attempts = j;
+      mark++;
+
+    }
   }
 }
+
+let userName = prompt('Can you type your name please');
+
+alert('Good job '+userName+' you have got '+mark+' out of 7, have a good day');
+
+console.log(attempts);
+
+
