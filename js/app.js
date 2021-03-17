8311c8adb192d9f81e768c91b7d87bc53f2598bc
fidaasaed <fidaa.saed97@gmail.com>
2021-03-17 17:21:42 +0200

    Fidaa's Changes

--- js/app.js
+++ js/app.js
@@ -1,134 +1,163 @@
 'use strict';
 let mark = 0;
 alert('Please answer the quistion by either typing yes or no, y or n');
-let favColor = prompt('Is black My favourite color?');
 
-if(favColor.toLowerCase() === 'yes' || favColor.toLowerCase() === 'y')
+favColor();
+pet();
+season();
+mType();
+mC();
+guessGame();
+guessGame2();
+
+function favColor() 
 {
-  alert('Correct, my favourite color is black');
-  // console.log('Correct, my favourite color is black');
-  mark++;
+  let favColor = prompt('Is black My favourite color?');
 
-}
-
-else if(favColor.toLowerCase() === 'no' || favColor.toLowerCase() === 'n')
-{
-  alert('Wrong, my favourite color is black');
-}
-
-else
-{
-  alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
-}
-
-let dogCat = prompt('I would rather have a cat than a dog as a pet?');
-
-if(dogCat.toLowerCase() === 'yes' || dogCat.toLowerCase() === 'y')
-{
-  alert('Wrong, my favourite pet is a dog');
-}
-
-else if(dogCat.toLowerCase() === 'no' || dogCat.toLowerCase() === 'n')
-{
-  alert('Correct, my favourite pet is a dog');
-  // console.log('Correct, my favourite pet is a dog');
-  mark++;
-
-}
-
-else
-{
-  alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
-}
-
-let summerWinter = prompt('I am a Summer person?');
-
-if(summerWinter.toLowerCase() === 'yes' || summerWinter.toLowerCase() === 'y')
-{
-  alert('Correct, my favourite season is Summer');
-  // console.log('Correct, my favourite season is Summer');
-  mark++;
-
-}
-
-else if(summerWinter.toLowerCase() === 'no' || summerWinter.toLowerCase() === 'n')
-{
-  alert('Wrong, my favourite season is Summer');
-}
-
-else
-{
-  alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
-}
-
-let musicType = prompt('My favourite music type is country?');
-
-if(musicType.toLowerCase() === 'yes' || musicType.toLowerCase() === 'y')
-{
-  alert('Wrong, my favourite music type is Rock and Roll');
-}
-
-else if(musicType.toLowerCase() === 'no' || musicType.toLowerCase() === 'n')
-{
-  alert('Correct, my favourite music type is Rock and Roll');
-  // console.log('Correct, my favourite music type is Rock and Roll');
-  mark++;
-
-}
-
-else
-{
-  alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
-}
-
-let missyClean = prompt('Am i a missy person');
-
-if(missyClean.toLowerCase() === 'yes' || missyClean.toLowerCase() === 'y')
-{
-  alert('Wrong, I am a Clean person');
-}
-
-else if(missyClean.toLowerCase() === 'no' || missyClean.toLowerCase() === 'n')
-{
-  alert('Correct, I am a Clean person');
-  // console.log('Correct, I am a Clean person');
-  mark++;
-
-}
-
-else
-{
-  alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
-}
-
-
-let userNumber = prompt('Can you guess what is my lucky number between 1 - 100');
-let noAttempt = 4;
-while(noAttempt > 1)
-{
-  if(parseInt(userNumber) === 50)
+  if(favColor.toLowerCase() === 'yes' || favColor.toLowerCase() === 'y')
   {
+    alert('Correct, my favourite color is black');
+    // console.log('Correct, my favourite color is black');
     mark++;
-    alert('Correct');
-    break;
+  
   }
-  else if(parseInt(userNumber) <= 40)
+  
+  else if(favColor.toLowerCase() === 'no' || favColor.toLowerCase() === 'n')
   {
-    alert('Too Low');
+    alert('Wrong, my favourite color is black');
   }
-  else if(parseInt(userNumber) >= 60)
-  {
-    alert('Too High');
-  }
+  
   else
   {
-    alert('invalid input');
+    alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
   }
-  noAttempt--;
-  userNumber = prompt('Can you guess what is my lucky number between 1 - 100, ' +noAttempt+' Attempt');
 }
 
-alert('Can you guess what language do i know');
+function pet()
+{
+  let dogCat = prompt('I would rather have a cat than a dog as a pet?');
+
+  if(dogCat.toLowerCase() === 'yes' || dogCat.toLowerCase() === 'y')
+  {
+    alert('Wrong, my favourite pet is a dog');
+  }
+  
+  else if(dogCat.toLowerCase() === 'no' || dogCat.toLowerCase() === 'n')
+  {
+    alert('Correct, my favourite pet is a dog');
+    // console.log('Correct, my favourite pet is a dog');
+    mark++;
+  
+  }
+  
+  else
+  {
+    alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
+  }
+}
+
+function season()
+{
+  let summerWinter = prompt('I am a Summer person?');
+
+  if(summerWinter.toLowerCase() === 'yes' || summerWinter.toLowerCase() === 'y')
+  {
+    alert('Correct, my favourite season is Summer');
+    // console.log('Correct, my favourite season is Summer');
+    mark++;
+
+  }
+
+  else if(summerWinter.toLowerCase() === 'no' || summerWinter.toLowerCase() === 'n')
+  {
+    alert('Wrong, my favourite season is Summer');
+  }
+
+  else
+  {
+    alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
+  }
+}
+
+function mType()
+{
+    let musicType = prompt('My favourite music type is country?');
+
+  if(musicType.toLowerCase() === 'yes' || musicType.toLowerCase() === 'y')
+  {
+    alert('Wrong, my favourite music type is Rock and Roll');
+  }
+
+  else if(musicType.toLowerCase() === 'no' || musicType.toLowerCase() === 'n')
+  {
+    alert('Correct, my favourite music type is Rock and Roll');
+    // console.log('Correct, my favourite music type is Rock and Roll');
+    mark++;
+
+  }
+
+  else
+  {
+    alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
+  }
+}
+
+function mC()
+{
+    let missyClean = prompt('Am i a missy person');
+
+  if(missyClean.toLowerCase() === 'yes' || missyClean.toLowerCase() === 'y')
+  {
+    alert('Wrong, I am a Clean person');
+  }
+
+  else if(missyClean.toLowerCase() === 'no' || missyClean.toLowerCase() === 'n')
+  {
+    alert('Correct, I am a Clean person');
+    // console.log('Correct, I am a Clean person');
+    mark++;
+
+  }
+
+  else
+  {
+    alert('Invalid Input, Please type yes or no, y or n, You can refresh the page to retake the quiz again ;)');
+  }
+}
+
+function guessGame()
+{
+  let userNumber = prompt('Can you guess what is my lucky number between 1 - 100');
+  let noAttempt = 4;
+  while(noAttempt > 1)
+  {
+    if(parseInt(userNumber) === 50)
+    {
+      mark++;
+      alert('Correct');
+      break;
+    }
+    else if(parseInt(userNumber) <= 40)
+    {
+      alert('Too Low');
+    }
+    else if(parseInt(userNumber) >= 60)
+    {
+      alert('Too High');
+    }
+    else
+    {
+      alert('invalid input');
+    }
+    noAttempt--;
+    userNumber = prompt('Can you guess what is my lucky number between 1 - 100, ' +noAttempt+' Attempt');
+  }
+}
+
+
+function guessGame2()
+{
+  alert('Can you guess what language do i know');
 let attempts = 6;
 let myAnswer = ['c#','html','css','javascript','java', 'sql'];
 //let userAnswer = prompt('type the correct answer \n (c# HTML CSS Java Javascript python SQl PHP');
@@ -147,6 +176,7 @@ for(let j = -5; j<attempts; j++)
 
     }
   }
+}
 }
 
 let userName = prompt('Can you type your name please');
